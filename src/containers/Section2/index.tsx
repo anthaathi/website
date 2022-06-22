3c1676f3b26e8b6e28b431d9b5ea63bcded14a70
Omkar Yadav <me@f22.dev>
2022-06-22 10:49:05 +0000

    feat: added data analysis in section2

--- src/containers/Section2/index.tsx
+++ src/containers/Section2/index.tsx
@@ -1,22 +1,23 @@
-import React from 'react';
-import {useStyletron} from 'baseui';
+import React from "react";
+import { useStyletron } from "baseui";
 import {
   HeadingMedium,
   HeadingSmall,
   HeadingXXLarge,
   LabelLarge,
   LabelMedium,
-} from 'baseui/typography';
-import {CompanyName} from '../../CompanyName';
+} from "baseui/typography";
+import { CompanyName } from "../../CompanyName";
 import {
   ApplicationMobile,
   ApplicationWeb,
   CloudServices,
   EdtLoop,
   ShoppingCatalog,
-} from '@carbon/icons-react';
-import {Button} from 'baseui/button';
-import {Block} from 'baseui/block';
+  AppConnectivity,
+} from "@carbon/icons-react";
+import { Button } from "baseui/button";
+import { Block } from "baseui/block";
 
 export function Section2() {
   const [css, $theme] = useStyletron();
@@ -25,57 +26,57 @@ export function Section2() {
     <div
       id="services"
       className={css({
-        maxHeight: '1400px',
-        width: '100%',
-        overflow: 'hidden',
+        maxHeight: "1400px",
+        width: "100%",
+        overflow: "hidden",
         [$theme.mediaQuery.medium]: {
-          maxHeight: 'none',
+          maxHeight: "none",
         },
         [$theme.mediaQuery.small]: {
-          maxHeight: 'none',
+          maxHeight: "none",
         },
-        backgroundImage: 'url(/Blue-BG.jpg)',
-        backgroundRepeat: 'none',
-        backgroundSize: 'cover',
+        backgroundImage: "url(/Blue-BG.jpg)",
+        backgroundRepeat: "none",
+        backgroundSize: "cover",
       })}
     >
       <div
         className={css({
-          background: 'rgba(19, 22, 92, 1)',
+          background: "rgba(19, 22, 92, 1)",
           [$theme.mediaQuery.large]: {
             background:
-              'linear-gradient(to left, rgba(19, 22, 92, 0.1) 50%, rgba(19, 22, 92, 0.9) 50%)',
+              "linear-gradient(to left, rgba(19, 22, 92, 0.1) 50%, rgba(19, 22, 92, 0.9) 50%)",
           },
         })}
       >
         <div
           className={css({
-            display: 'flex',
-            padding: '0 24px',
-            maxWidth: '1400px',
-            position: 'relative',
-            margin: '0 auto',
-            height: '100%',
-            flexDirection: 'column',
+            display: "flex",
+            padding: "0 24px",
+            maxWidth: "1400px",
+            position: "relative",
+            margin: "0 auto",
+            height: "100%",
+            flexDirection: "column",
             [$theme.mediaQuery.large]: {
-              flexDirection: 'row',
+              flexDirection: "row",
             },
           })}
         >
           <div
             className={css({
-              display: 'flex',
-              placeContent: 'center',
-              flexDirection: 'column',
-              width: '100%',
+              display: "flex",
+              placeContent: "center",
+              flexDirection: "column",
+              width: "100%",
               [$theme.mediaQuery.large]: {
-                width: '50%',
-                minHeight: '100vh',
+                width: "50%",
+                minHeight: "100vh",
               },
             })}
           >
-            <div className={css({paddingTop: '24px', paddingBottom: '48px'})}>
-              <LabelLarge $style={{color: '#F5B640'}}>WHAT WE DO</LabelLarge>
+            <div className={css({ paddingTop: "24px", paddingBottom: "48px" })}>
+              <LabelLarge $style={{ color: "#F5B640" }}>WHAT WE DO</LabelLarge>
               <HeadingXXLarge marginBottom="scale200" marginTop="scale200">
                 Our Services
               </HeadingXXLarge>
@@ -83,7 +84,7 @@ export function Section2() {
               <HeadingSmall
                 marginTop="scale200"
                 color="rgba(255,255,255,.8)"
-                maxWidth={['100%', '100%', '55%']}
+                maxWidth={["100%", "100%", "55%"]}
               >
                 {CompanyName} has its own Advanced Phishing Simulation tool
                 called Hooked for phishing simulation offering both a SAAS and
@@ -102,41 +103,42 @@ export function Section2() {
   );
 }
 
-const items = [
+const items: {
+  label: string;
+  icon: React.ComponentType<{ size: number; color: string; className: string }>;
+  content: string;
+}[] = [
   {
-    label: 'Web Development',
+    label: "Web Development",
     icon: ApplicationWeb,
-    image: '/web1.png',
     content:
-      'We do not just web design, but out-of-the box digital experiences that takes your business to the next level.',
+      "We do not just web design, but out-of-the box digital experiences that takes your business to the next level.",
   },
   {
-    label: 'Mobile Development',
+    label: "Mobile Development",
     icon: ApplicationMobile,
-    image: '/phn.png',
     content:
-      'We offer a full cycle of application design, integration and management services. Whether it is a consumer oriented app or a transformative enterprise-class solution, the company leads the entire mobile app development process from ideation and concept to delivery.',
+      "We offer a full cycle of application design, integration and management services. Whether it is a consumer oriented app or a transformative enterprise-class solution, the company leads the entire mobile app development process from ideation and concept to delivery.",
   },
   {
-    label: 'ECommerce Development',
+    label: "ECommerce Development",
     icon: ShoppingCatalog,
-    image: '/ecommerce.png',
-    content:
-      'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
+    content: "",
   },
   {
-    label: 'Cloud Services',
+    label: "Cloud Services",
     icon: CloudServices,
-    image: '/cloud1.png',
-    content:
-      'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
+    content: "",
   },
   {
-    label: 'DevSecOps Services',
+    label: "DevSecOps Services",
     icon: EdtLoop,
-    image: '/devops1.png',
-    content:
-      'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
+    content: "",
+  },
+  {
+    label: "Data-Analytics engineering",
+    icon: AppConnectivity,
+    content: "",
   },
 ];
 
@@ -146,13 +148,13 @@ function Items() {
   return (
     <div
       className={css({
-        display: 'grid',
-        gridTemplateColumns: '1fr',
+        display: "grid",
+        gridTemplateColumns: "1fr",
         [$theme.mediaQuery.large]: {
-          gridTemplateColumns: '1fr 1fr',
-          width: '50%',
+          gridTemplateColumns: "1fr 1fr",
+          width: "50%",
         },
-        width: '100%',
+        width: "100%",
       })}
     >
       <style jsx>{`
@@ -166,26 +168,26 @@ function Items() {
             key={index}
             className={
               css({
-                border: '1px solid rgb(255,255,255,.2)',
-                display: 'flex',
-                alignContent: 'center',
-                placeContent: 'center',
-                placeItems: 'center',
-                flexDirection: 'column',
-                cursor: 'pointer',
-                transitionProperty: 'all',
-                transitionDuration: '.2s',
-                transitionTimingFunction: 'ease',
-                ':hover': {
-                  backgroundColor: '#0095D6',
+                border: "1px solid rgb(255,255,255,.2)",
+                display: "flex",
+                alignContent: "center",
+                placeContent: "center",
+                placeItems: "center",
+                flexDirection: "column",
+                cursor: "pointer",
+                transitionProperty: "all",
+                transitionDuration: ".2s",
+                transitionTimingFunction: "ease",
+                ":hover": {
+                  backgroundColor: "#0095D6",
                 },
-              }) + ' items-wrapper'
+              }) + " items-wrapper"
             }
           >
             <value.icon
               size={54}
               color="#FFF"
-              className={css({paddingTop: '24px', paddingBottom: '24px'})}
+              className={css({ paddingTop: "24px", paddingBottom: "24px" })}
             />
             <HeadingMedium
               marginTop="scale200"
@@ -193,9 +195,9 @@ function Items() {
               marginBottom="0"
               maxWidth="80%"
               $style={{
-                textAlign: 'center',
-                marginLeft: 'auto',
-                marginRight: 'auto',
+                textAlign: "center",
+                marginLeft: "auto",
+                marginRight: "auto",
               }}
             >
               {value.label}
@@ -208,13 +210,13 @@ function Items() {
                 color="rgba(255,255,255,.8)"
                 marginBottom="scale600"
                 $style={{
-                  transitionProperty: 'all',
-                  transitionDuration: '.1s',
-                  transitionTimingFunction: 'ease',
+                  transitionProperty: "all",
+                  transitionDuration: ".1s",
+                  transitionTimingFunction: "ease",
                   [$theme.mediaQuery.large]: {
                     maxHeight: 0,
                   },
-                  overflow: 'hidden',
+                  overflow: "hidden",
                 }}
               >
                 {value.content}
